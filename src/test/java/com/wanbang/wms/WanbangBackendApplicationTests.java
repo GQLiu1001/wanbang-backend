82b3807541d2516c6e172d1258d5a37dcf9f6786
GQLiu1001 <moondust1196551872@gmail.com>
2025-02-20 15:46:09 +0800

    项目立项

---
+++ src/test/java/com/wanbang/wms/WanbangBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wanbang.wms;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WanbangBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
