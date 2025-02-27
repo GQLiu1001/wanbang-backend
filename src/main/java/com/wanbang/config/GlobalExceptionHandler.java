2116f315f8bc4eb325ae8b4f1cfdfefe736844c0
GQLiu1001 <moondust1196551872@gmail.com>
2025-02-27 13:31:00 +0800

    自定义异常拦截

--- src/main/java/com/wanbang/config/GlobalExceptionHandler.java
+++ src/main/java/com/wanbang/config/GlobalExceptionHandler.java
@@ -1,12 +1,23 @@
 package com.wanbang.config;
 
+import cn.dev33.satoken.exception.NotLoginException;
 import cn.dev33.satoken.util.SaResult;
 import com.wanbang.common.Result;
+import com.wanbang.common.ResultCode;
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
 @RestControllerAdvice
 public class GlobalExceptionHandler {
+
+    // 处理未登录异常
+    @ExceptionHandler(NotLoginException.class)
+    @ResponseStatus(HttpStatus.UNAUTHORIZED) // 返回 401 状态码
+    public Result handleNotLoginException(NotLoginException e) {
+        return Result.fail(ResultCode.UNAUTHORIZED);
+    }
     // 全局异常拦截
     @ExceptionHandler
     public Result handlerException(Exception e) {
